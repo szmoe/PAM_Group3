5f760bda526f2f3eba4b30a7038d146d17b0371b
szmoe <125073552+szmoe@users.noreply.github.com>
2025-06-13 21:01:37 +0200

    delete duplicate

--- Contribution/Contribution.R
+++ Contribution/Contribution.R
@@ -1,16 +1,12 @@
 library(tibble)
 
 project_contributions <- tibble::tibble(
-  Task = c("Introduction", "Method", "Result", "Discussion", "Conclusion"),
+  Topic = c("Introduction", "Method", "Result", "Discussion", "Conclusion"),
   Contributor = c("Malte", "Robin", "Moe", "Malte, Robin", "Moe")
 )
 
-project_contributions
+kable(project_contributions) %>%
+  kable_styling("striped", position = "left", font_size = 10)
 
-project_contributions <- data.frame(
-  Task = c("Introduction", "Method", "Result", "Discussion", "Conclusion"),
-  Contributor = c("Malte", "Robin", "Moe", "Malte, Robin", "Moe"),
-  stringsAsFactors = FALSE
-)
 
-project_contributions
+
